3601ab4a85c5b00ed8b23a9016c62062e83adfb8
Lyubomir Lalov <ldlalov@hotmail.com>
2022-08-10 11:37:04 +0300

    Add project files.

---
+++ 01. Odd Lines/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace _01._Odd_Lines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("../../../input.txt");
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                int row = 0;
+                while (line != null)
+                {
+                    if (row % 2 != 0)
+                    {
+                        Console.WriteLine(line);
+                    }
+                    row++;
+                    line = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ 03. Word Count/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _03._Word_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                                Dictionary<string, int> frequencies = new Dictionary<string, int>();
+
+            using (StreamReader reader = new StreamReader("../../../words.txt"))
+            {
+               string[] words = reader.ReadToEnd().Split();
+                using (StreamReader reader1 = new StreamReader("../../../input.txt"))
+                {
+                    string[] text = reader1.ReadToEnd().Split(' ', '\r','\n', '-',',','.');//Will be better with Regex
+                    foreach (var item in words)
+                    {
+                        if (!frequencies.ContainsKey(item.ToLower()))
+                        {
+                            frequencies.Add(item.ToLower(), 0);
+                        }
+
+                        foreach (var word in text)
+                        {
+                            if (word.ToLower() == item)
+                            {
+                                frequencies[item]++;
+                            }
+                        }
+                    }
+                }
+                foreach (var item in frequencies.OrderByDescending(x => x.Value))
+                {
+                    Console.WriteLine($"{item.Key} - {item.Value}");
+                }
+            }
+        }
+    }
+}
